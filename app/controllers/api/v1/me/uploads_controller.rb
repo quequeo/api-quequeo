d1af4ae1ac731570338d39bdaf20513ccb5190b8
Jaime García Méndez <jaimegarciamendez@gmail.com>
2024-12-11 16:40:42 -0300

    AWS region

--- app/controllers/api/v1/me/uploads_controller.rb
+++ app/controllers/api/v1/me/uploads_controller.rb
@@ -1,9 +1,8 @@
 class Api::V1::Me::UploadsController < ApplicationController
   def presigned_url
     s3 = Aws::S3::Resource.new
-    bucket = s3.bucket(ENV['AWS_S3_BUCKET_NAME'])
+    bucket = s3.bucket('quequeo')
     bucket_object = bucket.object(params[:file_name])
-    puts params
     url = bucket_object.presigned_url(
       :post, 
       expires_in: 3600, 
