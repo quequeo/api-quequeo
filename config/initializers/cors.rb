f5c4bb12a043a5f7eaf2b0ac51fd3513939d6562
Jaime García Méndez <jaimegarciamendez@gmail.com>
2025-01-09 12:02:06 -0300

    Cors update

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3001', 'http://localhost:5173', 'https://web.quequeo.com', 'https://main.d2c58utqvwb6hu.amplifyapp.com'
+    origins 'http://localhost:3001', 'http://localhost:5173', 'https://web.quequeo.com', 'https://resume.quequeo.com'
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
